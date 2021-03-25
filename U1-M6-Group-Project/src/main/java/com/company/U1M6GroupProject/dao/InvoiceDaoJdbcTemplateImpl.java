b31f2d34268d7637789a9b1faf6213d982561d1b
Michael Brown <michaelbrown@Michaels-Air.routerlogin.net>
2021-03-25 11:55:44 -0500

    Invoice changes to Template Impl

--- U1-M6-Group-Project/src/main/java/com/company/U1M6GroupProject/dao/InvoiceDaoJdbcTemplateImpl.java
+++ U1-M6-Group-Project/src/main/java/com/company/U1M6GroupProject/dao/InvoiceDaoJdbcTemplateImpl.java
@@ -1,12 +1,17 @@
 package com.company.U1M6GroupProject.dao;
 
 
+import com.company.U1M6GroupProject.model.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 @Repository
-public class InvoiceDaoJdbcTemplateImpl {
+public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao {
 
     private JdbcTemplate jdbcTemplate;
 
@@ -19,5 +24,46 @@ public class InvoiceDaoJdbcTemplateImpl {
             "select * from invoice where customerId = ?";
 
     @Autowired
+    public InvoiceDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
+    @Override
+    public Invoice createInvoice(Invoice invoice) {
+        jdbcTemplate.update(INSERT_INVOICE_SQL,
+                invoice.getInvoiceId(),
+                invoice.getCustomerId(),
+                invoice.getOrderDate(),
+                invoice.getPickupDate(),
+                invoice.getReturnDate(),
+                invoice.getLateFee());
+
+        int id = jdbcTemplate.queryForObject("select_invoice_id()", Integer.class);
+
+        invoice.setInvoiceId(id);
+
+        return invoice;
+    }
+
+    @Override
+    public void deleteInvoice(int id) {
+        jdbcTemplate.update(DELETE_INVOICE_SQL, id);
+    }
+
+    @Override
+    public List<Invoice> FindInvoiceByCustomer(String customer) {
+        return jdbcTemplate.query(SELECT_INVOICE_BY_CUSTOMER_SQL, this:: mapRowToInvoice, customer);
+    }
+
+    private Invoice mapRowToInvoice(ResultSet resultSet, int rowNumber) throws SQLException {
+        Invoice invoice1 = new Invoice();
+        invoice1.setInvoiceId(resultSet.getInt("invoiceId"));
+        invoice1.setCustomerId(resultSet.getInt("isbn"));
+        invoice1.setOrderDate(resultSet.getDate("orderDate").toLocalDate()); //why is this not working?
+        invoice1.setPickupDate(resultSet.getDate("pickupDate").toLocalDate());
+        invoice1.setReturnDate(resultSet.getDate("returnDate").toLocalDate());
+        invoice1.setLateFee(resultSet.getBigDecimal("lateFee"));
+
+        return invoice1;
+    }
 }

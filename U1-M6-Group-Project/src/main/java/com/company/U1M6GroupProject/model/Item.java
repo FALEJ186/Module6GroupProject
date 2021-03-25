7b6cd268d71323c986886039b5b6336b7fc1d41a
Frederick <fr.alejandro98@gmail.com>
2021-03-25 11:09:08 -0500

    completed item dao, template and model

--- U1-M6-Group-Project/src/main/java/com/company/U1M6GroupProject/model/Item.java
+++ U1-M6-Group-Project/src/main/java/com/company/U1M6GroupProject/model/Item.java
@@ -1,4 +1,67 @@
 package com.company.U1M6GroupProject.model;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Item {
+
+    private int id;
+    private String name;
+    private String description;
+    private BigDecimal dailyRate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getDailyRate() {
+        return dailyRate;
+    }
+
+    public void setDailyRate(BigDecimal dailyRate) {
+        this.dailyRate = dailyRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(name, item.name) && Objects.equals(description, item.description) && Objects.equals(dailyRate, item.dailyRate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, dailyRate);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", dailyRate=" + dailyRate +
+                '}';
+    }
 }

a66fb80d230daa985bafdbc5d21a0f3cfeafd273
Frederick <fr.alejandro98@gmail.com>
2021-03-25 15:31:26 -0500

    initial invoice item tests

--- U1-M6-Group-Project/src/test/java/com/company/U1M6GroupProject/dao/InvoiceItemDaoTest.java
+++ U1-M6-Group-Project/src/test/java/com/company/U1M6GroupProject/dao/InvoiceItemDaoTest.java
@@ -77,8 +77,8 @@ public class InvoiceItemDaoTest {
         invoiceItem.setInvoiceId(invoice.getId());
         invoiceItem.setItemId(item.getId());
         invoiceItem.setQuantity(4);
-        invoiceItem.setUnitRate(BigDecimal.valueOf(30.00));
-        invoiceItem.setDiscount(BigDecimal.valueOf(0.00));
+        invoiceItem.setUnitRate(new BigDecimal("30.00"));
+        invoiceItem.setDiscount(new BigDecimal("0.00"));
         invoiceItem = invoiceItemDao.addInvoiceItem(invoiceItem);
 
         assertEquals(invoiceItem, invoiceItemDao.getInvoiceItem(invoiceItem.getId()));
@@ -86,6 +86,37 @@ public class InvoiceItemDaoTest {
 
     @Test
     public void getInvoiceItem() {
+        Customer customer = new Customer();
+        customer.setFirstName("Test");
+        customer.setLastName("Check");
+        customer.setEmail("check@test.com");
+        customer.setCompany("Tesla");
+        customer.setPhone("000-000-0000");
+        customer = customerDao.addACustomer(customer);
+
+        Invoice invoice = new Invoice();
+        invoice.setCustomerId(customer.getId());
+        invoice.setOrderDate(LocalDate.of(2021, 3, 25));
+        invoice.setPickupDate(LocalDate.of(2021,3,26));
+        invoice.setReturnDate(LocalDate.of(2021,4,5));
+        invoice.setLateFee(BigDecimal.valueOf(100.00));
+        invoice = invoiceDao.addInvoice(invoice);
+
+        Item item = new Item();
+        item.setName("Test");
+        item.setDailyRate(BigDecimal.valueOf(19.99));
+        item.setDescription("test desc");
+        item = itemDao.addItem(item);
+
+        InvoiceItem invoiceItem = new InvoiceItem();
+        invoiceItem.setInvoiceId(invoice.getId());
+        invoiceItem.setItemId(item.getId());
+        invoiceItem.setQuantity(4);
+        invoiceItem.setUnitRate(new BigDecimal("30.00"));
+        invoiceItem.setDiscount(new BigDecimal("0.00"));
+        invoiceItem = invoiceItemDao.addInvoiceItem(invoiceItem);
+
+        assertEquals(invoiceItem, invoiceItemDao.getInvoiceItem(invoiceItem.getId()));
     }
 
     @Test

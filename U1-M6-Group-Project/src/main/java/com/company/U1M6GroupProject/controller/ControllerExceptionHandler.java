2abd3e479e8f73c31523cc2aaec8114f628c5256
Kanmani <govardh3@msu.edu>
2021-03-26 14:14:30 -0500

    invoice item controller

---
+++ U1-M6-Group-Project/src/main/java/com/company/U1M6GroupProject/controller/ControllerExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.company.U1M6GroupProject.controller;
+
+import com.company.U1M6GroupProject.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class}) //what exception im handling. anytime this exception shows up this will run
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<ErrorResponse> outOfRangeException(IllegalArgumentException e) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.toString(),e.getMessage());
+        ResponseEntity<ErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException e) {  //inside the brackets is called the generic?
+        ErrorResponse error = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.toString(),"Null pointer detected");
+        ResponseEntity<ErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<List<ErrorResponse>> handleRecordValidationError
+            (MethodArgumentNotValidException e)
+    {  //taking a list of error responses bc we have multiple validations for artist and album rn
+
+        // BindingResult holds the validation errors
+        BindingResult result = e.getBindingResult();
+
+        // Validation errors are stored in FieldError objects
+        List<FieldError> fieldErrors = result.getFieldErrors(); //instance variable sometimes called fields
+
+        // Translate the FieldErrors to CustomErrorResponse
+        List<ErrorResponse> errorResponseList = new ArrayList<>();
+
+        for (FieldError fieldError : fieldErrors) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.toString(), fieldError.getDefaultMessage());
+            errorResponse.setTimestamp(LocalDateTime.now());
+            errorResponse.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+            errorResponseList.add(errorResponse);
+        }
+
+        // Create and return the ResponseEntity
+        ResponseEntity<List<ErrorResponse>> responseEntity = new ResponseEntity<>(errorResponseList, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+
+
+    }
+
+}
